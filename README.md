d82a9b526894ed6abe5d66d3f72e0bce92cfa5c4
0x33c0unt <count@0x33c0unt.localdomain>
2021-02-06 16:24:49 +0100

    Dll Injection, Suspend/Resume, SetWindowHook

--- README.md
+++ README.md
@@ -1,14 +1,18 @@
 # Window Manager
 
-Linux like window manager for Windows Operating System. 
+Window manager for Windows Operating System. 
+
+Shortcuts;
 
 ALT+LBUTTON : MOVE
 
 ALT+RBUTTON : RESIZE
 
-ALT+T : Set on Top and Vice Versa
+ALT+CTRL+T : Set on Top and Vice Versa
 
-ALT+P : PAUSE/RESUME Process 
+ALT+CTRL+D : DLL Inject 
+
+ALT+CTRL+P : PAUSE/RESUME Process 
 
 ALT+CTRL+K : KILL Process
 
@@ -16,32 +20,20 @@ ALT+CTRL+I : KILL Window Manager Itself
 
 ALT+CTRL+L : Show Logs
 
+## Further description
+- It should be running with elevated privileges.
+- On the very first run, It asks for running on startup. In case of yes response, puts "WindowManager" key to "\HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\Run" and sets it value to executable path.
+- Checks any other instance running. 
+- Works by setting a callback for mouse and keyboard events.
+- Disables windows notification beep sound by setting a registry value at "AppEvents\Schemes\Apps\\.Default\\.Default\\.Current".
+- Disables window events while moving and resizing to prevent any kind of misclicks.
+- Exits from full screen on move/resize events.
+- Has a config file with 3 options
 
+    1. dll: 32-bit dll's path (for dll injection feature)
+    2. dll64: 64-bit dll's path (for dll injection feature)
+    3. first_run: indicator of first run or not
 
-## First week (18-24 January)
-- Completed 
-    1. First running version
-- Problems
-    1. Full screen windows cannot be moved before exiting from full screen.  [Solved]
-    2. Mouse clicks should be disabled while moving [Solved]
-    3. When window is disabled, every click causes system beep sound [Solved]
-    4. Solution2 causes some distruption for scenarios where user can use other ALT+MOUSE shortcuts e.g. File Properties shortcut is ALT + Clicking on File [Solved]
-- Solutions
-    1. ShowWindow API with SW_RESTORE parameter exits from Full Screen
-    2. EnableWindow API helps enabling/disabling window inputs
-    3. System beep sound path stored on Registery at `AppEvents\\Schemes\\Apps\\.Default\\.Default\\.Current`. We remove it meanwhile ALT key is on. And restore after.
-
-## TODO
-1. Basic setup
-2. Execute on startup option
-3. Performance optimization
-4. Prevent multiple instances
-5. Build instructions for README
-6. Better Handling on full Screen
-7. Check whether mouse pointer on the frame
-8. Deactivate if CTRL, SHIFT, TAB keys were pressed beside of ALT
-9. Additional Functions: Set window on top, pause, resume process 
-10. Consider using callback via SetWindowsHookExA instead of infinite while loop
 
 ## PREVIEW
 ![DEMO](a.gif)
--- Window Management Helper/main.cpp
+++ Window Management Helper/main.cpp
@@ -4,29 +4,287 @@
 #include <stdio.h>
 #include <iostream>
 #include <list>
-
-
-POINT lastPoint;
-int lastTick;
-
-
-
 #include <tchar.h>
+#include <fstream>
+#include <sstream>
 
 #define MAX_KEY_LENGTH 255
 #define MAX_VALUE_NAME 16383
+using namespace std;
 
+struct _threads
+{
+	THREADENTRY32 te32[256];
+	DWORD count = 0;
+};
+typedef struct _threads threads;
 
-HWND hConsole;
+HWND console_handle;
 
+HANDLE handle;
 char windowText[256];
 char windowProcess[256];
 
-using namespace std;
+
+POINT last_point;
+int last_tick;
+
+//config
+string dll_path;
+string dll64_path;
+bool first_run;
+
 
 list<HWND> hWnds;
+HHOOK keyboard_hook;
+HHOOK mouse_hook;
+KBDLLHOOKSTRUCT kbdStruct;
+MSLLHOOKSTRUCT mbdStruct;
 
-string GetLastErrorAsString()
+void keyboard_events(DWORD key);
+void mouse_events(DWORD key, MSLLHOOKSTRUCT msInfo);
+char* get_window_text(HWND hWnd);
+char* get_window_process(HWND hWnd);
+
+void enable_windows()
+{
+	list <HWND>::iterator i;
+	for (i = hWnds.begin(); i != hWnds.end(); i++)
+		EnableWindow(*i, 1);
+	hWnds.clear();
+}
+
+DWORD hwnd_to_pid(HWND hWnd)
+{
+	DWORD dwProcessId;
+	GetWindowThreadProcessId(hWnd, &dwProcessId);
+	return dwProcessId;
+}
+
+LRESULT __stdcall keyboard_hook_callback(int nCode, WPARAM wParam, LPARAM lParam)
+{
+	if (nCode >= 0)
+	{
+		
+		if (wParam == WM_SYSKEYDOWN || wParam == WM_KEYDOWN )
+		{
+			kbdStruct = *((KBDLLHOOKSTRUCT*)lParam);
+		    if (GetAsyncKeyState(VK_LCONTROL) & 0x8000 && GetAsyncKeyState(VK_LMENU) & 0x8000 && !((GetAsyncKeyState(VK_SHIFT) & 0x8000) | (GetAsyncKeyState(VK_TAB) & 0x8000)))
+				keyboard_events(kbdStruct.vkCode);
+			
+		}
+	}
+	return CallNextHookEx(keyboard_hook, nCode, wParam, lParam);
+}
+
+LRESULT __stdcall mouse_hook_callback(int nCode, WPARAM wParam, LPARAM lParam)
+{
+	if (nCode >= 0)
+	{
+			mbdStruct = *((MSLLHOOKSTRUCT*)lParam);
+			if ((wParam == 0x200 || wParam == 0x201) && GetAsyncKeyState(VK_LMENU) & 0x8000 && !((GetAsyncKeyState(VK_SHIFT) & 0x8000) | (GetAsyncKeyState(VK_TAB) & 0x8000)))
+				mouse_events(wParam, mbdStruct);
+			//else if (wParam == 0x202)
+				
+	}
+	return CallNextHookEx(mouse_hook, nCode, wParam, lParam);
+}
+
+void set_hook()
+{
+	if (!(keyboard_hook = SetWindowsHookEx(WH_KEYBOARD_LL, keyboard_hook_callback, GetModuleHandle(NULL), 0)))
+	{
+		MessageBox(NULL, "Failed to install hook!", "Error", MB_ICONERROR);
+	}
+	if (!(mouse_hook = SetWindowsHookEx(WH_MOUSE_LL, mouse_hook_callback, GetModuleHandle(NULL), 0)))
+	{
+		MessageBox(NULL, "Failed to mouse hook!", "Error", MB_ICONERROR);
+	}
+}
+
+void release_hook()
+{
+	UnhookWindowsHookEx(keyboard_hook);
+	UnhookWindowsHookEx(mouse_hook);
+}
+
+threads* ListProcessThreads(DWORD dwOwnerPID)
+{
+	threads* _threads = (threads*)malloc(sizeof(threads));
+	HANDLE hThreadSnap = INVALID_HANDLE_VALUE;
+
+	hThreadSnap = CreateToolhelp32Snapshot(TH32CS_SNAPTHREAD, 0);
+	if (hThreadSnap == INVALID_HANDLE_VALUE)
+	{
+		//_convertions.printError("ListProcessThreads1");
+		return _threads;
+	}
+
+	int i = 0;
+	_threads->te32[i].dwSize = sizeof(THREADENTRY32);
+	if (!Thread32First(hThreadSnap, &_threads->te32[i]))
+	{
+		//_convertions.printError("ListProcessThreads2");
+		CloseHandle(hThreadSnap);
+		return _threads;
+	}
+	do
+	{
+		if (_threads->te32[i].th32OwnerProcessID == dwOwnerPID)
+		{
+			//printf("\n     THREAD ID      = 0x%08X\n", _threads.te32[i].th32ThreadID);
+			i++;
+		}
+		if (i == 256)
+		{
+			print("Maximum Thread Count Exceeded");
+			break;
+		}
+		_threads->te32[i].dwSize = sizeof(THREADENTRY32);
+	} while (Thread32Next(hThreadSnap, &_threads->te32[i]));
+
+	CloseHandle(hThreadSnap);
+	_threads->count = i;
+	return _threads;
+}
+
+BOOL ResumeProcess(DWORD pid)
+{
+	threads* _threads = ListProcessThreads(pid);
+
+	for (DWORD i = 0; i < _threads->count; i++)
+	{
+		HANDLE hThread = OpenThread(THREAD_SUSPEND_RESUME, NULL, _threads->te32[i].th32ThreadID);
+		if (hThread == NULL)
+		{
+			free(_threads);
+			return false;
+		}
+		if (ResumeThread(hThread) == -1)
+		{
+			free(_threads);
+			return false;
+		}
+	}
+	free(_threads);
+	return true;
+}
+
+BOOL SuspendProcess(DWORD pid)
+{
+	threads* _threads = ListProcessThreads(pid);
+	for (DWORD i = 0; i < _threads->count; i++)
+	{
+		HANDLE hThread = OpenThread(THREAD_SUSPEND_RESUME, NULL, _threads->te32[i].th32ThreadID);
+		if (hThread == NULL)
+		{
+			free(_threads);
+			return false;
+		}
+		if (SuspendThread(hThread) == -1)
+		{
+			free(_threads);
+			return false;
+		}
+	}
+	free(_threads);
+	return true;
+
+}
+
+DWORD inject_basic(HWND hWnd)
+{
+	
+	DWORD dwProcessId = hwnd_to_pid(hWnd);;
+	
+	handle = OpenProcess(PROCESS_CREATE_THREAD | PROCESS_SET_INFORMATION | PROCESS_VM_WRITE | PROCESS_QUERY_INFORMATION | PROCESS_VM_OPERATION, 0, dwProcessId);
+
+	PVOID dll_path_adr = VirtualAllocEx(handle, NULL, 1024, MEM_COMMIT | MEM_RESERVE, PAGE_EXECUTE_READWRITE);
+	if (dll_path_adr == NULL)
+	{
+		print("VirtualAllocEx returned 0");
+		return 0;
+	}
+	BOOL is32bit;
+
+	IsWow64Process(handle, &is32bit);
+	const char* Dll;
+
+	if (is32bit)
+	{
+		if (dll_path.empty())
+		{
+			print("no dll found (32 bit)");
+			return 0;
+		}
+		Dll = dll_path.c_str();
+	}
+	else
+	{
+		if (dll64_path.empty())
+		{
+			print("no dll found (64 bit)");
+			return 0;
+		}
+		Dll = dll64_path.c_str();
+	}
+
+
+	print("%x %s", is32bit, Dll);
+
+	WriteProcessMemory(handle, dll_path_adr, Dll, strlen(Dll), NULL);
+
+	HANDLE hThread = CreateRemoteThread(handle, NULL, 0, (LPTHREAD_START_ROUTINE)&LoadLibraryA, dll_path_adr, NULL, 0);
+	if (hThread == NULL)
+		return 0;
+
+	DWORD r;
+	WaitForSingleObject(hThread, INFINITE);
+	GetExitCodeThread(hThread, &r);
+
+	get_window_process(hWnd);
+
+	if (r > 0)
+		print("%s injected to %s, Base: %x", (char*)(Dll + string(Dll).find_last_of("\\/") + 1), windowProcess, r);
+	else
+		print("Dll Injection Failed, further info;\nDll: %s\nProcess: %s", Dll, windowProcess);
+
+	VirtualFreeEx(handle, dll_path_adr, 0, MEM_RELEASE);
+	CloseHandle(handle);
+
+	return r;
+}
+
+
+void run_on_startup()
+{
+	char path[MAX_PATH];
+	GetModuleFileNameA(nullptr, path, MAX_PATH);
+	HKEY hkey = NULL;
+	LONG createStatus = RegCreateKey(HKEY_CURRENT_USER, "SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", &hkey); //Creates a key       
+	LONG status = RegSetValueEx(hkey, "WindowManager", 0, REG_SZ, (BYTE*)path, (strlen(path) + 1) * sizeof(wchar_t));
+}
+
+bool read_config()
+{
+	ifstream infile("config.txt");
+	string line;
+
+	while (getline(infile, line))
+	{
+
+		if (!strncmp("dll=", line.c_str(), sizeof("dll=")-1))
+			dll_path = line.substr(sizeof("dll=")-1);
+		else if(!strncmp("dll64=", line.c_str(), sizeof("dll64=")-1))
+			dll64_path = line.substr(sizeof("dll64=")-1);
+		else if (!strncmp("first_run=", line.c_str(), sizeof("first_run=") - 1))
+			first_run = atoi(line.substr(sizeof("first_run=") - 1).c_str());
+	}
+	infile.close();
+	return true;
+}
+
+string get_last_error_as_string()
 {
 	DWORD errorMessageID = ::GetLastError();
 	if (errorMessageID == 0) {
@@ -45,15 +303,15 @@ string GetLastErrorAsString()
 	return message;
 }
 
-char* getWindowText(HWND hWnd)
+char* get_window_text(HWND hWnd)
 {
 	GetWindowTextA(hWnd, windowText, 255);
 	return windowText;
 }
 
-char* getWindowProcess(HWND hWnd)
+char* get_window_process(HWND hWnd)
 {
-	DWORD pid;
+	DWORD pid = 0;
 	PROCESSENTRY32 entry;
 	entry.dwSize = sizeof(PROCESSENTRY32);
 	HANDLE snapshot = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
@@ -74,7 +332,8 @@ char* getWindowProcess(HWND hWnd)
 	}
 	return 0;
 }
-char* getWindowProcessFullPath(HANDLE h)
+
+char* get_window_process_full_path(HANDLE h)
 {
 	DWORD size = 255;
 	QueryFullProcessImageNameA(h, 0, windowProcess, &size);
@@ -82,8 +341,7 @@ char* getWindowProcessFullPath(HANDLE h)
 }
 
 
-
-void disableWindows(HWND hWnd)
+void disable_windows(HWND hWnd)
 {
 	bool found = false;
 	list <HWND>::iterator i;
@@ -106,44 +364,56 @@ HKEY hKey;
 DWORD buffersize = 1024;
 char* lpData = new char[buffersize];
 
-void regGetDefaultBeep()
+void reg_get_default_beep()
 {
 	RegOpenKeyExA(HKEY_CURRENT_USER, "AppEvents\\Schemes\\Apps\\.Default\\.Default\\.Current", NULL, KEY_READ, &hKey);
 	RegQueryValueExA(hKey, "", NULL, NULL, (LPBYTE)lpData, &buffersize);
 	RegCloseKey(hKey);
 }
 
-void regSetDefaultBeep(char* newData)
+void reg_set_default_beep()
 {
 	RegOpenKeyExA(HKEY_CURRENT_USER, "AppEvents\\Schemes\\Apps\\.Default\\.Default\\.Current", NULL, KEY_SET_VALUE, &hKey);
-	RegSetValueA(hKey, "", REG_SZ, newData, 0);
+	RegSetValueA(hKey, "", REG_SZ, "", 0);
 	RegCloseKey(hKey);
 }
 
+void exit_full_screen(HWND hWnd, int width, int height, RECT& rect, POINT point)
+{
+	int newWidth = width / 2;
+	int newHeight = height / 2;
+	int newX = point.x - newWidth/2;
+	int newY = point.y - newHeight/2;
+	int flags = SWP_NOZORDER;
+	ShowWindow(hWnd, SW_RESTORE);
+	while (IsZoomed(hWnd));
+
+	print("%s Exited from Full Screen", get_window_text(hWnd));
+	SetWindowPos(
+		hWnd,
+		NULL,
+		newX,
+		newY,
+		newWidth,
+		newHeight,
+		flags
+	);
+	GetWindowRect(hWnd, &rect);
+	
+}
 void move(HWND hWnd, int width, int height, RECT rect, POINT point)
 {
-	disableWindows(hWnd);
 	int newX = 0;
 	int newY = 0;
 	int newWidth = 0;
 	int newHeight = 0;
-	int flags = SWP_NOZORDER;
-	if (IsZoomed(hWnd))
-	{
-		ShowWindow(hWnd, SW_RESTORE);
-		while (IsZoomed(hWnd));
-		newWidth = width / 2;
-		newHeight = height / 2;
-		flags |= SWP_NOMOVE;
-		print("%s Exited from Full Screen", getWindowText(hWnd));
-	}
-	else {
+	int flags = SWP_NOZORDER | SWP_NOSIZE;
+	
 		newWidth = width;
 		newHeight = height;
-		newX = rect.left + (point.x - lastPoint.x);
-		newY = rect.top + (point.y - lastPoint.y);
-		print("Moving %s to X: %d Y: %d", getWindowText(hWnd), newX, newY);
-	}
+		newX = rect.left + (point.x - last_point.x);
+		newY = rect.top + (point.y - last_point.y);
+		print("Moving %s to X: %d Y: %d", get_window_text(hWnd), newX, newY);
 
 	SetWindowPos(
 		hWnd,
@@ -158,10 +428,10 @@ void move(HWND hWnd, int width, int height, RECT rect, POINT point)
 
 void resize(HWND hWnd, int width, int height, RECT rect, POINT point)
 {
-	disableWindows(hWnd);
-	int newWidth = width + (point.x - lastPoint.x);
-	int newHeight = height + (point.y - lastPoint.y);
-	print("Resizing: %s to Width: %d Height: %d %", getWindowText(hWnd), newWidth, newHeight);
+	int newWidth = width + (point.x - last_point.x);
+	int newHeight = height + (point.y - last_point.y);
+	int flags = SWP_NOZORDER;
+	print("Resizing: %s to Width: %d Height: %d", get_window_text(hWnd), newWidth, newHeight);
 	SetWindowPos(
 		hWnd,
 		NULL,
@@ -169,15 +439,15 @@ void resize(HWND hWnd, int width, int height, RECT rect, POINT point)
 		rect.top,
 		newWidth,
 		newHeight,
-		0x40
+		flags
 	);
 }
 
-void setOnTop(HWND hWnd)
+void set_on_top(HWND hWnd)
 {
 	if (GetWindowLong(hWnd, GWL_EXSTYLE) & WS_EX_TOPMOST)
 	{
-		print("%s on not Top", getWindowText(hWnd));
+		print("%s on not Top", get_window_text(hWnd));
 		SetWindowPos(
 			hWnd,
 			HWND_NOTOPMOST,
@@ -189,7 +459,7 @@ void setOnTop(HWND hWnd)
 		);
 	}
 	else {
-		print("%s on Top", getWindowText(hWnd));
+		print("%s on Top", get_window_text(hWnd));
 		SetWindowPos(
 			hWnd,
 			HWND_TOPMOST,
@@ -202,9 +472,9 @@ void setOnTop(HWND hWnd)
 	}
 }
 
-inline void printLastError()
+inline void print_last_error()
 {
-	print("%s", GetLastErrorAsString().c_str());
+	print("%s", get_last_error_as_string().c_str());
 }
 
 bool kill(HWND hWnd)
@@ -214,131 +484,184 @@ bool kill(HWND hWnd)
 	HANDLE h = OpenProcess(PROCESS_TERMINATE | PROCESS_QUERY_INFORMATION, 0, pid);
 	if (h == NULL)
 	{
-		printLastError();
+		print_last_error();
 		return false;
 	}
+	get_window_process(hWnd);
 	if (TerminateProcess(h, 0))
-	{
-		if (getWindowProcess(hWnd))
-			print("%s is Terminated", windowProcess);
-		else
-		{
-			printLastError();
-			return false;
-		}
-	}
+		print("%s is Terminated", windowProcess);
 	else
 	{
-		printLastError();
+		print_last_error();
 		return false;
 	}
 	return true;
 }
 
-void test()
+BOOL is_running(DWORD pid)
 {
-	HWND hWnd = GetForegroundWindow();
-	DWORD pid;
-	GetWindowThreadProcessId(hWnd, &pid);
-	HANDLE h = OpenProcess(PROCESS_QUERY_INFORMATION, 1, pid);
-	getWindowProcess(hWnd);
+	threads* _threads = ListProcessThreads(pid);
+	if (_threads->te32[0].th32ThreadID == GetCurrentThreadId())
+		return true;
+
+	HANDLE hThread = OpenThread(THREAD_SUSPEND_RESUME, NULL, _threads->te32[0].th32ThreadID);
+	if (!hThread)
+		return true;
+
+	int r = SuspendThread(hThread);
+	int rr = ResumeThread(hThread);
+	if (r > 0)
+		return false;
+
+	return true;
 }
 
-void keyboardEvents(HWND hWnd)
+void suspend_resume(HWND hWnd)
 {
-	if (GetAsyncKeyState('T') && !IsZoomed(hWnd))
+	DWORD pid = hwnd_to_pid(hWnd);
+
+	if (is_running(pid))
 	{
-		setOnTop(hWnd);
-		Sleep(300);
+		if (SuspendProcess(pid))
+			print("Suspended process: %s", get_window_process(hWnd));
 	}
-	else if (GetAsyncKeyState('K') && GetAsyncKeyState(VK_LCONTROL) & 0x8000)
+	else
+		if(ResumeProcess(pid))
+			print("Resumed process: %s", get_window_process(hWnd));
+}
+void keyboard_events(DWORD key)
+{
+	HWND hWnd = GetForegroundWindow();
+
+	if (key == 'I')
 	{
-		kill(hWnd);
-		Sleep(300);
-	}
-	else if (GetAsyncKeyState('I') && GetAsyncKeyState(VK_LCONTROL) & 0x8000)
+		release_hook();
 		exit(0);
-	else if (GetAsyncKeyState('B') && GetAsyncKeyState(VK_LCONTROL) & 0x8000)
-		test();
-	else if (GetAsyncKeyState('L') && GetAsyncKeyState(VK_LCONTROL) & 0x8000)
+	}
+	else if (key == 'L')
 	{
-		int r = GetWindowLong(hConsole, GWL_STYLE);
+		int r = GetWindowLong(console_handle, GWL_STYLE);
 		if (r & WS_VISIBLE)
-			ShowWindow(hConsole, SW_HIDE);
+			ShowWindow(console_handle, SW_HIDE);
 		else
 		{
-			ShowWindow(hConsole, SW_SHOW);
-			BringWindowToTop(hConsole);
+			ShowWindow(console_handle, SW_SHOW);
+			SetForegroundWindow(console_handle);
 		}
-		Sleep(300);
 	}
+
+	if (hWnd == console_handle)
+		return;
+
+	if (key == 'T' && !IsZoomed(hWnd))
+	{
+		set_on_top(hWnd);
+	}
+	else if (key == 'K')
+	{
+		kill(hWnd);
+	}
+	else if (key == 'D')
+		inject_basic(hWnd);
+	else if (key == 'P')
+		suspend_resume(hWnd);
+
 }
-void mouseEvents(HWND hWnd)
+void mouse_events(DWORD key, MSLLHOOKSTRUCT msInfo)
 {
+	HWND hWnd = GetForegroundWindow();
+	if (hWnd == console_handle)
+		return;
 	RECT rect;
 	GetWindowRect(hWnd, &rect);
 	int width = rect.right - rect.left;
 	int height = rect.bottom - rect.top;
 
-	POINT point;
-	if (GetCursorPos(&point)) {
-		if (GetTickCount() - lastTick > 100)
-			lastPoint = point;
-		bool position_change = ((point.x - lastPoint.x) || (point.y - lastPoint.y));
-		if (position_change)
-		{
-			if (GetAsyncKeyState(VK_LBUTTON) & 0x8000)
-				move(hWnd, width, height, rect, point);
-			else if (GetAsyncKeyState(VK_RBUTTON) & 0x8000)
-				resize(hWnd, width, height, rect, point);
-		}
-		lastPoint = point;
-		lastTick = GetTickCount();
+	POINT point = msInfo.pt;
+	if (GetTickCount() - last_tick > 100)
+		last_point = point;
+	bool position_change = ((point.x - last_point.x) || (point.y - last_point.y));
+	if (position_change)
+	{
+		if (IsWindowEnabled(hWnd))
+			disable_windows(hWnd);
+
+		if (IsZoomed(hWnd) && GetAsyncKeyState(VK_LBUTTON) & 0x8000 )
+			exit_full_screen(hWnd, width, height, rect, point);
+		if (GetAsyncKeyState(VK_LBUTTON) & 0x8000)
+			move(hWnd, width, height, rect, point);
+		else if (GetAsyncKeyState(VK_RBUTTON) & 0x8000)
+			resize(hWnd, width, height, rect, point);
+
+		enable_windows();
 	}
+	
+	last_point = point;
+	last_tick = GetTickCount();
 }
 
+int WINAPI msg_handler(int param1)
+{
+	set_hook();
+	MSG Msg;
+	while (GetMessage(&Msg, NULL, 0, 0) > 0)
+	{
+		TranslateMessage(&Msg);
+		DispatchMessage(&Msg);
+	}
+	return 1;
+}
 
-bool beep = true;
+bool is_already_open()
+{
+	HANDLE event_handle = CreateEvent(NULL, true, false, "WindowManager");
+	if (GetLastError() == ERROR_ALREADY_EXISTS) 
+		return true;
+	return false;
+}
 
 int main()
 {
+	if (is_already_open())
+	{
+		MessageBox(0, "An instance is already running", "Window Manager", 0);
+		return 1;
+	}
+
+	read_config();
+
+	console_handle = GetConsoleWindow();
+	if (first_run)
+	{
+		ShowWindow(console_handle, SW_SHOW);
+		print("Do you want to run this app on startup?");
+		char answer = getchar();
+		if (answer == 'y' || answer == 'Y')
+			run_on_startup();
+		
+		ofstream out("config.txt");
+		out << "dll=" << dll_path << endl;
+		out << "dll64=" << dll64_path << endl;
+		out << "first_run=" << 0 << endl;
+		out.close();
+	}
+	else
+		ShowWindow(console_handle, SW_HIDE);
+
 	SetConsoleTitle("Window Manager - 0x33c0unt");
-	hConsole = GetConsoleWindow();
-	ShowWindow(hConsole, SW_HIDE);
-	regGetDefaultBeep();
 
-	char empty = '\0';
+	reg_get_default_beep();
+	
+	reg_set_default_beep();
 
+	CreateThread(NULL, NULL, (LPTHREAD_START_ROUTINE)&msg_handler, NULL, NULL, NULL);
+
+	char cmd[256];
 	while (true)
 	{
-		Sleep(1);
-		HWND hWnd = GetForegroundWindow();
-		
-		if (GetAsyncKeyState(VK_LMENU) && !GetAsyncKeyState(VK_SHIFT)  && !GetAsyncKeyState(VK_TAB))
-		{
-			if (beep)
-			{
-				regSetDefaultBeep(&empty);
-				beep = false;
-			}
-			
-			mouseEvents(hWnd);
-
-			keyboardEvents(hWnd);
-		}
-		else
-		{
-			list <HWND>::iterator i;
-			for (i= hWnds.begin(); i != hWnds.end(); i++)
-				EnableWindow(*i, 1);
-			hWnds.clear();
-			if (!beep)
-			{
-				regSetDefaultBeep(lpData);
-				beep = true;
-			}
-		}
-
+		int r = scanf_s("%s", cmd, 255);
+		if (!strcmp(cmd, "exit"))
+			exit(0);
 	}
 }
 
